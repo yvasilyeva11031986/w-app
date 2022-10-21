b37a1344405b5b6c60e955c0a0b2451312db1a25
yvasilyeva11031986 <113214120+yvasilyeva11031986@users.noreply.github.com>
2022-10-21 09:02:57 +0100

    Date fixes done
    ++

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if(hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if(minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = [
+        "SUN",
+        "MON",
+        "TUE",
+        "WED",
+        "THU",
+        "FRI",
+        "SAT",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemp(response) {
     //console.log(response.data.temperature.current);
     let temperatureElement = document.querySelector("#temp");
@@ -10,7 +34,11 @@ function displayTemp(response) {
     humidityElement.innerHTML = response.data.temperature.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
-    
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.time * 1000);
+    // let date = new Date(response.data.time * 1000);
+    // console.log(date.getDay)
+    // console.log(date);
 }
 
 let apiKey = "f17boc23c2b2f34d1ab3d2t904991752";
